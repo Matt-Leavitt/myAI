7b6d4bd88528db7ac636532f479826cecd55f85c
Matt-Leavitt <mattleavittjr@gmail.com>
2025-02-17 16:10:52 -0500

    Change Ai name to Alfred
    Changed AI name to Alfred

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Matt Leavitt`;
 export const OWNER_DESCRIPTION: string = `An MBA candidate at UNC`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Alfred`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
