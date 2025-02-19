42d32d9740402ae003ec98f1a64c6d27a59b3221
Matt-Leavitt <mattleavittjr@gmail.com>
2025-02-18 20:03:01 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Matt's AI Recruiting Butler`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Copyright 2025 Matt Leavitt`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Matt's AI Recruiting Butler`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
